dc45ff94322a01327125a0c3422548722d6cbdbc
Manuel Stephano Chavez Anton <105468040+Darkas-Overgold@users.noreply.github.com>
2024-12-09 16:38:55 -0500

    Delete 9/Excepciones/Procesamiento de datos directory

--- 9/Excepciones/Procesamiento de datos/readTxt.py
+++
@@ -1,6 +0,0 @@
-# Abre el archivo en modo lectura con codificación UTF-8
-with open("Procesamiento de datos\\test.txt", encoding="UTF-8") as archivo:
-# Lee el contenido del archivo
-    contenido = archivo.read()
-    # Imprime el contenido del archivo
-    print(contenido)
